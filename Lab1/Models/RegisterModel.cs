9b595d1d9e3df7335e9f86014a7087a8c2d0f73f
savely.volodkov@gmail.com <savely.volodkov@gmail.com>
2022-03-04 20:39:36 +0300

    Validation for RegisterModel.cs ready

--- Lab1/Models/RegisterModel.cs
+++ Lab1/Models/RegisterModel.cs
@@ -1,51 +1,87 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Lab1.Models;
 
 
 public class RegisterModel : IValidatableObject
 {
-    [Required(ErrorMessage = "Не указан Email")]
+    [Required]
     [Display(Name = "Email")]
     [RegularExpression(".+@.+\\..+", ErrorMessage = "Please Enter Correct Email Address")]
     public string Email { get; set; }
     
     [Required]
     [DataType(DataType.Text)]
+    [Display(Name = "First Name")]
     public string FirstName { get; set; }
     
     [Required]
+    [Display(Name = "Last Name")]
     public string LastName { get; set; }
     [Required]
+    [Display(Name = "Second Name")]
     public string SecondName { get; set; }
-    [Required]
+    [Display(Name = "Series And Passport Number")]
     public string SeriesAndPassportNumber { get; set; }
-    [Required]
+    
+    [Display(Name = "Identification Number")]
     public string IdentificationNumber { get; set; }
+    
     [Required]
+    [Display(Name = "Phone Number")]
     [DataType(DataType.PhoneNumber)]
     public string PhoneNumber { get; set; }
     
-    [Required(ErrorMessage = "Не указан пароль")]
+    [Required(ErrorMessage = "The Password field is required")]
+    [Display(Name = "Password")]
     [DataType(DataType.Password)]
-    [StringLength(30, MinimumLength = 6, ErrorMessage = "min 6, max 30")]
+    [StringLength(30, MinimumLength = 6, ErrorMessage = "Length of Password should be between 6 and 30 letters")]
     public string Password { get; set; }
  
     [DataType(DataType.Password)]
-    [Compare("Password", ErrorMessage = "Пароль введен неверно")]
+    [Display(Name = "Confirm Password")]
+    [Compare("Password", ErrorMessage = "Passwords doesn't match")]
     public string ConfirmPassword { get; set; }
 
     public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
     {
         List<ValidationResult> errors = new List<ValidationResult>();
-        if (this.Password.Length < 5)
+        if (this.PhoneNumber[0]!='+')
         {
-            errors.Add(new ValidationResult("Password leng"));
+            errors.Add(new ValidationResult("Phone Number should start with '+'"));
         }
-        if (this.PhoneNumber.Length < 5)
+        if (this.PhoneNumber[1..].Any(x => char.IsLetter(x)))
         {
-            errors.Add(new ValidationResult("PhoneNumber leng"));
+            errors.Add(new ValidationResult("Phone Number can't contain letters"));
         }
+        
+        if (this.FirstName.Any(x => char.IsDigit(x)))
+        {
+            errors.Add(new ValidationResult("First Name can't contain digits"));
+        }
+        if (this.LastName.Any(x => char.IsDigit(x)))
+        {
+            errors.Add(new ValidationResult("Last Name can't contain digits"));
+        }
+        if (this.SecondName.Any(x => char.IsDigit(x)))
+        {
+            errors.Add(new ValidationResult("Second Name can't contain digits"));
+        }
+
+        if (this.IdentificationNumber[..6].Any(x => char.IsLetter(x)) ||
+            char.IsDigit(this.IdentificationNumber[7]) || 
+            this.IdentificationNumber[^2..^1].Any(x => char.IsDigit(x)))
+        {
+            errors.Add(new ValidationResult("Passport Identification number is not correct"));
+        }
+        if (this.SeriesAndPassportNumber.Length != 9)
+        {
+            errors.Add(new ValidationResult("Passport serie and number are not correct"));
+        }
+        
+        
+        
         return errors;
     }
 }
