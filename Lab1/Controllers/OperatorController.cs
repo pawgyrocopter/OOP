9903870a1b9842ebc5a9007473c400beab3258a4
savely.volodkov@gmail.com <savely.volodkov@gmail.com>
2022-04-13 20:18:56 +0300

    operator undo created

---
+++ Lab1/Controllers/OperatorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab1.Controllers;
+
+public class OperatorController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
